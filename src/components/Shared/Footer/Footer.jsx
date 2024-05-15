540c5d81c88a0b80fbbcefcfabd3e4caec643bdd
Adarsh Mishra <adarshformplus@gmail.com>
2024-05-15 07:21:24 +0530

    first commit

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -55,15 +55,15 @@ const Footer = () => {
 								<div className="footer-contact-info">
 									<div className="footer-address">
 										<span><i className="fas fa-map-marker-alt"></i></span>
-										<p> 121, Mirzapure Union office,<br /> Sylhet, Bangladesh 03214 </p>
+										<p> Om Vihar<br /> Ghaziabad,India,201009 </p>
 									</div>
 									<p>
 										<i className="fas fa-phone-alt"></i>
-										+88 017 51 040425
+										+918920661951
 									</p>
 									<p className="mb-0">
 										<i className="fas fa-envelope"></i>
-										ujjalzaman@gmail.com
+										adarshformplus@gmail.com
 									</p>
 								</div>
 							</div>
